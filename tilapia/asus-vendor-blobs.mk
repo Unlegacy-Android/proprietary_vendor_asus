11ed20dfcce6725d1df24ea5a768accd0d260d9a
André Pinela <sheffzor@gmail.com>
2015-12-24 15:04:55 +0000

    tilapia: add apn-conf list
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- tilapia/asus-vendor-blobs.mk
+++ tilapia/asus-vendor-blobs.mk
@@ -1,4 +1,5 @@
 VENDOR_FOLDER := vendor/asus/tilapia
 
 PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libxgold-ril.so:system/vendor/lib/libxgold-ril.so
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libxgold-ril.so:system/vendor/lib/libxgold-ril.so \
+	$(VENDOR_FOLDER)/proprietary/etc/apns-conf.xml:system/etc/apns-conf.xml
