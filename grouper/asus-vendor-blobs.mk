fec1905b640e029795d08ae36b62c9a2d8dc5cd8
Ziyan <jaraidaniel@gmail.com>
2017-02-05 14:04:08 +0100

    Add grouper-specific files from vendor/nvidia, remove duplicate camera hal
    Change-Id: Idea3e499a6a3668a07556d5690e417d838ac9df4

--- grouper/asus-vendor-blobs.mk
+++ grouper/asus-vendor-blobs.mk
@@ -1,6 +1,7 @@
 VENDOR_FOLDER := vendor/asus/grouper
 
 PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/camera.tegra3.so:system/vendor/lib/hw/camera.tegra3.so \
+	$(VENDOR_FOLDER)/proprietary/etc/nvcamera.conf:system/etc/nvcamera.conf \
+	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors.grouper.so:system/vendor/lib/hw/sensors.grouper.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.lightsensor.so:system/vendor/lib/libsensors.lightsensor.so
