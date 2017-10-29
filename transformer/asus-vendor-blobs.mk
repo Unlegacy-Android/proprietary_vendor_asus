6beb96177c6b98689f391a2f7a5e71dfe327fed1
Svyatoslav Ryhel <clamor95@gmail.com>
2017-10-29 20:32:27 +0200

    transformer: add sensor lib
    Change-Id: I392e56e5cd93adb40f1ea00407ca4f03e4a81d48
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- transformer/asus-vendor-blobs.mk
+++ transformer/asus-vendor-blobs.mk
@@ -12,4 +12,14 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/EC/DOCK-EC20N-0207.rom:system/etc/firmware/EC/DOCK-EC20N-0207.rom \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/EC/DOCK-EC21N-0105.rom:system/etc/firmware/EC/DOCK-EC21N-0105.rom \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/EC/OPEN-PAD-0216.rom:system/etc/firmware/EC/OPEN-PAD-0216.rom \
-	$(VENDOR_FOLDER)/proprietary/etc/firmware/EC/FU-d.cfg:system/etc/firmware/EC/FU-d.cfg
+	$(VENDOR_FOLDER)/proprietary/etc/firmware/EC/FU-d.cfg:system/etc/firmware/EC/FU-d.cfg \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors.transformer.so:system/vendor/lib/hw/sensors.transformer.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors_generic.transformer.so:system/vendor/lib/hw/sensors_generic.transformer.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors6050.transformer.so:system/vendor/lib/hw/sensors6050.transformer.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libami.so:system/vendor/lib/libami.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libami_sensor_mw.so:system/vendor/lib/libami_sensor_mw.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.al3010.so:system/vendor/lib/libsensors.al3010.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.isl29018.so:system/vendor/lib/libsensors.isl29018.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.isl29028.so:system/vendor/lib/libsensors.isl29028.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.ltr558als.so:system/vendor/lib/libsensors.ltr558als.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensorservice.so:system/vendor/lib/libsensorservice.so
