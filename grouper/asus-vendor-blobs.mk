eb438ca32fa6d9a83675a6e1dd43c4615de36ba6
Dániel Járai <jaraidaniel@gmail.com>
2017-12-27 20:12:06 +0100

    grouper: add ODM blobs
    These used to live in the common repo, but since they're device
    specific, just keep them here.
    
    Change-Id: Icb3342e9b3f7051950d1d9235c1896d12d7c4ee5
    (cherry picked from commit 412c3a0efd97b660e01cfbd6db2d51cb5754d031)

--- grouper/asus-vendor-blobs.mk
+++ grouper/asus-vendor-blobs.mk
@@ -4,4 +4,6 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/etc/nvcamera.conf:system/etc/nvcamera.conf \
 	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors.grouper.so:system/vendor/lib/hw/sensors.grouper.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvodm_imager.so:system/vendor/lib/libnvodm_imager.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libnvodm_query.so:system/vendor/lib/libnvodm_query.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.lightsensor.so:system/vendor/lib/libsensors.lightsensor.so
