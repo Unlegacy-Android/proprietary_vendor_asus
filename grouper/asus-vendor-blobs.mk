82ab2c92343ad2a3a265389a3d667c12bce05647
André Pinela <sheffzor@gmail.com>
2018-01-21 20:16:43 +0000

    Start building open-source libsensors
    Change-Id: Ic1fc29d7b72304d9244bf097d4348075a54959ca
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- grouper/asus-vendor-blobs.mk
+++ grouper/asus-vendor-blobs.mk
@@ -2,6 +2,4 @@ VENDOR_FOLDER := vendor/asus/grouper
 
 PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/etc/nvcamera.conf:system/etc/nvcamera.conf \
-	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors.grouper.so:system/vendor/lib/hw/sensors.grouper.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.lightsensor.so:system/vendor/lib/libsensors.lightsensor.so
+	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt
